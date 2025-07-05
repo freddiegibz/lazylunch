b37d25ba727e111ffb841ddebd34591ffc2f5d8a
freddiegibz <your-email@example.com>
2025-07-05 13:55:34 -0500

    Fix Typescript Error

--- _error.tsx
+++ _error.tsx
@@ -1,16 +1,22 @@
-function Error({ statusCode }) {
-    return (
-      <p style={{ textAlign: 'center', marginTop: '2rem' }}>
-        {statusCode
-          ? `An error ${statusCode} occurred on server`
-          : 'An error occurred on client'}
-      </p>
-    )
-  }
-  
-  Error.getInitialProps = ({ res, err }) => {
-    const statusCode = res ? res.statusCode : err ? err.statusCode : 404
-    return { statusCode }
-  }
-  
-  export default Error
+import { NextPageContext } from 'next'
+
+interface ErrorProps {
+  statusCode?: number
+}
+
+function Error({ statusCode }: ErrorProps) {
+  return (
+    <p style={{ textAlign: 'center', marginTop: '2rem' }}>
+      {statusCode
+        ? `An error ${statusCode} occurred on server`
+        : 'An error occurred on client'}
+    </p>
+  )
+}
+
+Error.getInitialProps = ({ res, err }: NextPageContext) => {
+  const statusCode = res ? res.statusCode : err ? err.statusCode : 404
+  return { statusCode }
+}
+
+export default Error
